99e6b7466c0d3677786da729fdc267897fc6e663
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2019-09-16 14:14:44 -0500

    adf

--- Fallout_of_Color/Scenes/Enemies/BehindEnemies.gd
+++ Fallout_of_Color/Scenes/Enemies/BehindEnemies.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var gravity = 2;
+var motion = Vector2();
+const GROUND = Vector2(0,-1);
+export var speed = 10;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +11,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	motion.y += gravity;
+	move_and_slide(motion, GROUND);
