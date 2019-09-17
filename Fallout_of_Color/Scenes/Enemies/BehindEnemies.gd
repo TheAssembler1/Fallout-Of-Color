14566146da38e46dea7cab7ca957826205f88ae6
Nerdwerd <50635875+Nerdwerd@users.noreply.github.com>
2019-09-16 19:11:58 -0500

    Enemy

--- Fallout_of_Color/Scenes/Enemies/BehindEnemies.gd
+++ Fallout_of_Color/Scenes/Enemies/BehindEnemies.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var gravity = 2;
 var motion = Vector2();
 const GROUND = Vector2(0,-1);
-export var speed = 10;
+export var speed = 100;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +12,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	motion.y += gravity;
+	if ColorPlayer.transform.x > transform.x:
+		motion.x = speed
+	elif ColorPlayer.transform.x < transform.x:
+		motion.x = -speed
 	move_and_slide(motion, GROUND);
