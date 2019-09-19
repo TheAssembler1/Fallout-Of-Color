33dd8502ee05eea6274f3878e062e349b39b8bee
TheAssembler1 <lewisnoah592@gmail.com>
2019-09-19 18:18:14 -0500

    Started making the main scene

--- Fallout_of_Color/Scripts/ColorPlayer.gd
+++ Fallout_of_Color/Scripts/ColorPlayer.gd
@@ -7,6 +7,9 @@ var left = false
 var right = false
 var grounded = false
 
+#says if the player is alive
+var dead = false
+
 #booleans for effect the block
 var rot_left = false
 var rot_right = false
@@ -19,6 +22,12 @@ export var gravity = 2
 export var speed = 400
 export var jump_speed = 400
 var rotate_speed = 10
+
+#checks if player has it the ground yet
+var hit_ground = 0
+
+#variables for if player is going right or left
+export var hit_wall = false
 
 #vectors for motion
 var motion = Vector2()
@@ -41,64 +50,76 @@ func _process(delta):
 #getting the player input
 func Input(delta):
 	
-	#applying gravity
-	motion.y += gravity * delta
-	
-	#setting grounded
-	grounded = is_on_floor()
-	
-	#setting booleans for input
-	right = Input.is_action_pressed("ui_right")
-	left = Input.is_action_pressed("ui_left")
-	down = Input.is_action_pressed("ui_down")
-	up = Input.is_action_just_pressed("ui_up")
-
-	
-	#going left and right
-	if left:
-		motion.x = -speed * delta
-		$Particles.emitting = true
-		$Particles.position.x = 16
-		$Particles.scale.x = -1
-	elif right:
-		motion.x = speed * delta
-		$Particles.emitting = true
-		$Particles.position.x = -16
-		$Particles.scale.x = 1
-	else:
-		$Particles.emitting = false
-		motion.x = 0
-	
-	if left && up && grounded:
-		rot_left = true
-		motion.y += -jump_speed
-		prev_degree = $Sprite.rotation_degrees
-	elif right && up && grounded:
-		rot_right = true
-		motion.y += -jump_speed
-		prev_degree = $Sprite.rotation_degrees
-	elif up && grounded:
-		rot_right = false
-		rot_left = false
-		motion.y += -jump_speed
-
+	if dead == false:
+		#applying gravity
+		motion.y += gravity * delta
 		
+		#setting grounded
+		grounded = is_on_floor()
 		
-	if rot_left && $Sprite.rotation_degrees >= int(prev_degree-80):
-		$Sprite.rotation_degrees -= int(rotate_speed)
-		$Sprite.rotation_degrees = int($Sprite.rotation_degrees)
-	elif rot_right && $Sprite.rotation_degrees <= int(prev_degree+80):
-		$Sprite.rotation_degrees += int(rotate_speed)
-		$Sprite.rotation_degrees = int($Sprite.rotation_degrees)
-	else:
-		if int($Sprite.rotation_degrees)%90 != 0:
-			var distance = int($Sprite.rotation_degrees)%90
-			var prev = $Sprite.rotation_degrees
-			$Sprite.rotation_degrees += distance
+		#setting booleans for input
+		right = Input.is_action_pressed("ui_right")
+		up = Input.is_action_just_pressed("ui_up")
+	
+		if is_on_wall():
+			speed *= -1
+			hit_wall = true
+		else:
+			hit_wall = false
+		
+		#going left and right
+		if hit_ground > 50:
+			if speed < 0:
+				motion.x = speed * delta
+				$Particles.emitting = true
+				$Particles.position.x = 16
+				$Particles.scale.x = -1
+			elif speed > 0:
+				motion.x = speed * delta
+				$Particles.emitting = true
+				$Particles.position.x = -16
+				$Particles.scale.x = 1
+				
+		if speed < 0 && up && grounded:
+			rot_left = true
+			motion.y += -jump_speed
+			prev_degree = $Sprite.rotation_degrees
+		elif speed > 0 && up && grounded:
+			rot_right = true
+			motion.y += -jump_speed
+			prev_degree = $Sprite.rotation_degrees
+		elif up && grounded:
+			rot_right = false
+			rot_left = false
+			motion.y += -jump_speed
+	
+			
+		if rot_left && $Sprite.rotation_degrees >= int(prev_degree-80):
+			$Sprite.rotation_degrees -= int(rotate_speed)
+			$Sprite.rotation_degrees = int($Sprite.rotation_degrees)
+		elif rot_right && $Sprite.rotation_degrees <= int(prev_degree+80):
+			$Sprite.rotation_degrees += int(rotate_speed)
+			$Sprite.rotation_degrees = int($Sprite.rotation_degrees)
+		else:
 			if int($Sprite.rotation_degrees)%90 != 0:
-				$Sprite.rotation_degrees = prev
-				$Sprite.rotation_degrees -= distance
-		rot_right = false
-		rot_left = false
+				var distance = int($Sprite.rotation_degrees)%90
+				var prev = $Sprite.rotation_degrees
+				$Sprite.rotation_degrees += distance
+				if int($Sprite.rotation_degrees)%90 != 0:
+					$Sprite.rotation_degrees = prev
+					$Sprite.rotation_degrees -= distance
+			rot_right = false
+			rot_left = false
+		
+		hit_ground += 1
+		motion = move_and_slide(motion, normal_floor)
+
+func _on_Area2D_body_entered(body):
 	
-	motion = move_and_slide(motion, normal_floor)
+	if body.is_in_group("RedBlock"):
+		$Particles.emitting = false
+		$Sprite.visible = false
+		$DeathParticles.emitting = true
+		dead = true
+		
+	pass # Replace with function body.
--- Fallout_of_Color/Scripts/GameController.gd
+++ Fallout_of_Color/Scripts/GameController.gd
@@ -1,17 +1,25 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#boundaries of spawning blocks are
+#x = -272 && 872
+#y = 560
+
+#checks if player has it a wall
+onready var hit_wall = get_node("ColorPlayer").hit_wall
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.wait_time = 30;
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+	
+	var hit_wall = get_node("ColorPlayer").hit_wall
+	
+	#makes the payer turn
+	if hit_wall:
+		print_debug("Hit the wall")
+	
+	pass
